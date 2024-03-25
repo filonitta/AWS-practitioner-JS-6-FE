986b283848c73859cb2beec6bb1515dfac747274
Nataliia Shyriaieva <Nataliia_Shyriaieva@epam.com>
2024-03-25 15:01:57 +0200

    feat: add endpoint to fetch products

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -17,17 +17,10 @@ export default function Products() {
 
   return (
     <Grid container spacing={4}>
-      {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-      {data.map(({ count, ...product }, index) => (
+      {data.map(({ count, ...product }) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
-          <Card
-            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
-          >
-            <CardMedia
-              sx={{ pt: "56.25%" }}
-              image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
-            />
+          <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+            <CardMedia sx={{ pt: "56.25%" }} image={product.image} title="Image title" />
             <CardContent sx={{ flexGrow: 1 }}>
               <Typography gutterBottom variant="h5" component="h2">
                 {product.title}
