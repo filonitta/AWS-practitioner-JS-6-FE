ca61a3644c6be52b636e13e5994def1bb115c32e
Nataliia Shyriaieva <Nataliia_Shyriaieva@epam.com>
2024-03-25 13:30:49 +0200

    initial

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	resolve: {
+		alias: {
+			'~': path.resolve(__dirname, 'src'),
+		},
+	},
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/setupTests.ts',
+	},
+	server: {
+		port: 3017,
+	},
+	build: { chunkSizeWarningLimit: 1600 },
+});
